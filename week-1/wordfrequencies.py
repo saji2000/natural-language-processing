58f18a626b78bbda3ce7bf1d7a7a86cfb0892ccd
saji2000 <sajaddaneshmand79@gmail.com>
2025-08-26 23:17:04 -0400

    Frequency analysis of words

--- week-1/wordfrequencies.py
+++ week-1/wordfrequencies.py
@@ -4,9 +4,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 from preprocessing import preprocess_data
 
-nltk.download('twitter_samples')
+# check if twitter_samples exists
+try:
+    nltk.data.find('corpora/twitter_samples')
+except LookupError:
+    nltk.download('twitter_samples')
 
-nltk.download('stopwords')
+# check if stopwords exists
+try:
+    nltk.data.find('corpora/stopwords')
+except LookupError:
+    nltk.download('stopwords')
 
 # select the lists of positive and negative tweets
 all_positive_tweets = twitter_samples.strings('positive_tweets.json')
@@ -17,6 +25,8 @@ tweets = all_positive_tweets + all_negative_tweets
 
 # let's see how many tweets we have
 print("Number of tweets: ", len(tweets))
+
+labels = np.append(np.ones((len(all_positive_tweets), 1)), np.zeros((len(all_negative_tweets), 1)), axis=0)
 
 def build_freqs(tweets, ys):
     yslist = np.squeeze(ys).tolist()
@@ -30,3 +40,49 @@ def build_freqs(tweets, ys):
                 freqs[pair] += 1
             else:
                 freqs[pair] = 1
+    return freqs
+
+freqs = build_freqs(tweets, labels)
+
+print("type of freqs: ", type(freqs))
+
+print("length of freqs: ", len(freqs.keys()))
+
+print("some examples from freqs: \n", list(freqs.items())[:10])
+
+keys = ['happi', 'fuck', 'nice', 'good', 'bad', 'sad', 'mad', 'best', 'pretti',
+        '❤', ':)', ':(', '😒', '😬', '😄', '😍', '♛',
+        'song', 'idea', 'power', 'play', 'magnific']
+
+data = []
+
+for word in keys:
+    pos = 0
+    neg = 0
+
+    if (word, 1) in freqs:
+        pos = freqs[(word, 1)]
+    if (word, 0) in freqs:
+        neg = freqs[(word, 0)]
+
+    data.append((word, pos, neg))
+
+
+fig, ax = plt.subplots(figsize=(10, 5))
+
+# convert positive raw counts to logarithmic scale. we add 1 to avoid log(0)
+x = np.log([x[1] + 1 for x in data])  
+
+# do the same for the negative counts
+y = np.log([x[2] + 1 for x in data]) 
+
+ax.scatter(x, y)
+
+plt.xlabel('Log Positive Count')
+plt.ylabel('Log Negative Count')
+
+for i in range(len(data)):
+    ax.annotate(data[i][0], (x[i], y[i]))
+
+ax.plot([0, 9], [0, 9], color = "red")  # dashed line y=x
+plt.show()
